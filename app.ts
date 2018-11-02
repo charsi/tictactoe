f697db4f734c6a13cee1990e35c441112f9a311d
Nishil <nishilfoo@gmail.com>
2018-11-02 11:44:20 -0700

    readline

--- app.ts
+++ app.ts
@@ -1,10 +1,24 @@
-const readline = require('readline');
-import {Board, BoxStatus} from './Board';
+import {TicTacGame, BoxStatus} from './TicTacGame';
 
-let tictac = new Board();
+const readline = require('readline');
+
+
+let tictac = new TicTacGame(3);
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
 tictac.draw();
 
-tictac.go(1,1,"X");
+while(!tictac.finished()){ // game still on
+    rl.prompt('enter position:', (answer:number[])=>{
+        tictac.go(answer[0],answer[1],"X");
+        rl.close();
+    });
+}
 
-tictac.go(1,0,"O");
+//tictac.go(1,1,"X");
+
+//tictac.go(1,0,"O");
